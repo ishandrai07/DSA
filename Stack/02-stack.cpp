0cfdbba0b24a1f8d756a974cfddb48f5b3171e47
ishand <mywork4ever.77@gmail.com>
2026-09-20 15:21:15 +0530

    implementing Stack using C++ STL

---
+++ Stack/02-stack.cpp
@@ -0,0 +1,20 @@
+/*implementing Stack using C++ STL */
+
+#include<iostream>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+int main(){
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    while(!s.empty()){
+        cout<<s.top() << endl;
+        s.pop();
+    }
+    return 0;
+}
